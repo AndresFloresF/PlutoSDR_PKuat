50c621b14a92185f4a6ab2f921769e0eb479802c
AndresFloresKotoUAT <andresdavid_2000@hotmail.com>
2024-10-03 18:40:21 -0600

    Actualizacion Temporal

--- CP2112/BMP180_CP2112.c
+++ CP2112/BMP180_CP2112.c
@@ -3,9 +3,12 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "SLABHIDDevice.h"
+#include "SLABCP2112.h"
 
 int main(int argc, char *argv[])
 {
+    int *connectedDevice;
     const __u_short VID = 0x10C4;
     const __u_short PID = 0xEA90;
     const __u_short I2C_BMP180 = 0x20;
@@ -24,7 +27,21 @@ int main(int argc, char *argv[])
     
     printf("Device found\n");
 
+    HidSmbus_Open(connectedDevice,0,VID,PID);
+    HidSmbus_SetSmbusConfig(connectedDevice,100000, 0X02,0,100,100,0,2);
+    HidSmbus_GetGpioConfig(connectedDevice,0x20,0x20,0x06,0xFF);
+    HidSmbus_WriteLatch(connectedDevice,0,0x20);
+
     
+
+    printf("Puerto Configurado \n");
+
+    HidSmbus_WriteRequest(connectedDevice, 0x20 , unsigned char byteArray[] {0xF4, 0X2E},2);
+    delay(5);
+    HidSmbus_AddressReadRequest(connectedDevice,0x20,);
+
+
+    HidSmbus_Close(connectedDevice);
 
     return 0;
 }
